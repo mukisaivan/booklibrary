794850ab5dfd56d3593841937955c1f6c4bc9868
mukisaivan <mukisaivan340@gmail.com>
2022-06-10 03:22:49 +0300

    made some edits june 10

--- base/views.py
+++ base/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+bookshelfs = [
+    {'id': 1, 'name': 'lets learn python'},
+    {'id': 2, 'name': 'design with me'},
+    {'id': 3, 'name': 'front end developer'},
+]
+
+
+def home(request):
+    context = {'bookshelfs': bookshelfs}
+    return render(request, 'base/home.html', context)
+
+def bookshelf(request,pk):
+    bookshelf = None
+    for i in bookshelfs:
+        if i['id'] == int(pk):
+            bookshelf = i
+    context = {'bookshelf': bookshelf}
+    return render(request, 'base/bookshelf.html', context)
 # Create your views here.
