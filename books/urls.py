d46db82770d1e5b094ff8616f48ff066934f9b52
melissa <melissaukech@gmail.com>
2022-06-20 10:37:36 +0300

    work

---
+++ books/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('Home page')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home), 
+]
+
+
