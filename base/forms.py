3d12e40c2256d115f8536c8ecab1b1651ed8527d
mukisaivan <mukisaivan340@gmail.com>
2022-07-08 16:32:18 +0300

    8/7/2022

--- base/forms.py
+++ base/forms.py
@@ -1,21 +1,21 @@
 from django.forms import ModelForm
-from .models import Bookshelf, Book, User
-from django import forms
+from .models import Bookshelf
+# from django import forms
 
 
-class Bookshelfform(forms.ModelForm):
+class Bookshelfform(ModelForm):
     class Meta:
         model = Bookshelf
         fields = '__all__'
 
 
-class Bookform(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = '__all__'
-
-
-class UserForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = '__all__'
+# class Bookform(forms.ModelForm):
+#     class Meta:
+#         model = Book
+#         fields = '__all__'
+#
+#
+# class UserForm(forms.ModelForm):
+#     class Meta:
+#         model = User
+#         fields = '__all__'
